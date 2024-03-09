e41691dd6d995e855d50dc286de6dba8bbf6e8f4
rowan-turcotte <45rowan.turcotte@gmail.com>
2024-03-09 03:28:06 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import GetEthBalance from "get_eth_balance";
+import Web3 from 'web3';
+import Web3HttpProvider from 'web3-providers-http';
+
+const DEFAULT_RPC = 'https://eth1.lava.build/lava-referer-c371dc95-09e8-4749-9091-b5c66b4342b3/'
+
+class RowanTurcotte0Eth {
+    async getBalance(address, libName = undefined) {
+        try {
+            if (libName) {
+                const LIB = new (eval(libName))()
+                return await LIB.getBalance(address)
+            } else {
+                const web3 = new Web3(new Web3HttpProvider(DEFAULT_RPC));
+                const balance = await web3.eth.getBalance(address)
+                return web3.utils.fromWei(balance, 'ether')
+            }
+        } catch (e) {
+            console.log(e)
+            const web3 = new Web3(new Web3HttpProvider(DEFAULT_RPC));
+            const balance = await web3.eth.getBalance(address)
+            return web3.utils.fromWei(balance, 'ether')
+        }
+    }
+}
+export default RowanTurcotte0Eth;
